35419b10a9c1696032d68ae0d1d99f18c280417f
Anas-Kharboutli <anas.kharboutli@gmail.com>
2024-12-03 13:48:55 +0100

    initial repo push for Airbnb-clone

---
+++ app/layout.tsx
@@ -0,0 +1,48 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import next from "next";
+import { Nunito } from "next/font/google";
+import Navbar from "./components/navbar/Navbar";
+import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./components/providers/ToasterProvider";
+
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+}); 
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "AirBnB",
+  description: "Property Booking App",
+};
+
+const font = Nunito({
+  subsets: ["latin"]
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={font.className}
+      >
+        <ToasterProvider />
+        <RegisterModal />
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
