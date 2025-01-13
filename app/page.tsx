23a6a39b49734ed6f48ceabe04254802781bfc69
Anas-Kharboutli <anas.kharboutli@gmail.com>
2025-01-13 10:29:36 +0100

    solving DynamicServerError for deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+export const dynamic = 'force-dynamic';
+
 import getCurrentUser from "./actions/getCurrentUser";
 import getListings, { IListingsParams } from "./actions/getListings";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/listings/ListingCard";
 
-export const dynamic = "force-dynamic";
 
 interface HomeProps {
   searchParams: IListingsParams;
